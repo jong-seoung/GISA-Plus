18ed1fe9629d58708a9930570f81c4cddec7b2e0
beak jong seoung <jjong015189@gmail.com>
2024-08-22 15:48:28 +0900

    TopNav 구현

--- gisa-frontend/src/App.js
+++ gisa-frontend/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import { Container } from "react-bootstrap";
+
+import TopNav from "./components/TopNav";
+import { StatusProvider } from "./contexts/StatusContext";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <TopNav />
+        <Container>
+          <Outlet />
+          <hr />
+        </Container>
+      </>
+    ),
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StatusProvider>
+      <RouterProvider router={router} />
+    </StatusProvider>
   );
 }
 
