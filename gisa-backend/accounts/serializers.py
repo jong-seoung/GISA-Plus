c897160aedff44e79e10a2a016d5253d226a851b
beak jong seoung <jjong015189@gmail.com>
2024-08-22 18:21:04 +0900

    로그인 시 id대신 username으로 받도록 수정

--- gisa-backend/accounts/serializers.py
+++ gisa-backend/accounts/serializers.py
@@ -23,13 +23,12 @@ class SignupSerializer(serializers.ModelSerializer):
 
 
 class LoginSerializer(serializers.Serializer):
-    email = serializers.EmailField()
+    username = serializers.CharField()
     password = serializers.CharField(write_only=True)
 
     def validate(self, data):
         user = authenticate(**data)
         if user and user.is_active:
-            self.context['request'].user = user
             update_last_login(None, user)
             return user
         raise serializers.ValidationError("Unable to log in with provided credentials.")
