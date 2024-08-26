5fa1f5c34e7c8796508f55bc720547386d6be294
beak jong seoung <jjong015189@gmail.com>
2024-08-26 17:32:41 +0900

    데일리 문제 Detail

--- gisa-frontend/src/components/DailyQuiz.jsx
+++ gisa-frontend/src/components/DailyQuiz.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useApiAxios } from "../api";
 import { useParams } from "react-router-dom";
 import {
   Container,
@@ -12,30 +13,30 @@ import {
 import styles from "./DailyQuiz.module.css"; // 모듈 CSS 임포트
 
 function DailyQuiz() {
-  const { categoryName } = useParams(); // URL 경로에서 categoryName을 가져옴
+  const [{ data: origQuiz = undefined, loading }, refetch] =
+    useApiAxios(`/quiz/api/post/0`);
   const [quiz, setQuiz] = useState([]);
-  const [showAnswers, setShowAnswers] = useState(false); // 정답 표시 여부를 관리하는 상태
-  const [loading, setLoading] = useState(true);
+  const { categoryName } = useParams();
+  const [showAnswers, setShowAnswers] = useState(false);
 
   useEffect(() => {
-    const sampleQuiz = [
-      {
-        question: "오늘의 첫 번째 퀴즈는?",
-        answers: ["정답1", "정답2", "정답3"],
-        correctAnswer: "정답1",
-      },
-    ];
-
-    setQuiz(sampleQuiz);
-    setLoading(false);
-  }, []);
+    setShowAnswers(false);
+    setQuiz(origQuiz || []);
+  }, [origQuiz]);
 
   const handleShowAnswers = () => {
-    setShowAnswers(true); // 버튼 클릭 시 정답 표시 상태를 true로 변경
+    setShowAnswers(true);
   };
 
-  if (loading) {
-    return <p>로딩 중...</p>;
+  const handleNextPage = () => {
+    refetch({
+      url: `/quiz/api/post/${quiz.id}`,
+      method: "GET",
+    });
+  };
+  console.log(quiz);
+  if (loading || !quiz || !quiz.unit || !quiz.unit.category) {
+    return <p></p>;
   }
 
   return (
@@ -45,75 +46,72 @@ function DailyQuiz() {
           <Card className="mb-4">
             <Card.Body>
               <Row className="align-items-center">
-                {/* 이전 버튼 */}
-                <Col xs={1}>
-                  <Button className={styles.Background} variant="link">
-                    <i className={styles.arrow} role="img"></i>
-                  </Button>
-                </Col>
+                {/* 왼쪽 여백 */}
+                <Col xs={1} />
 
                 {/* 퀴즈 내용 */}
                 <Col xs={10}>
                   <Row>
-                    {/* 제목 및 날짜 섹션 */}
                     <Col className="text-center">
-                      <h3>{categoryName} - 단원 </h3>
-                      <p> version </p>
+                      <h4>
+                        {categoryName}, {quiz.unit.name}
+                      </h4>
+                      <small>{quiz.unit.category.version}</small>
                     </Col>
                   </Row>
 
                   <Row className="mt-4">
-                    {/* 문제 섹션 */}
                     <Col className="text-left fw-bold h4">
-                      <p>{quiz[0].question}</p>
+                      <p>{quiz.title}</p>
                     </Col>
 
-                    {/* 이미지 섹션 */}
                     <Row className="text-center">
-                      <img
-                        src={quiz[0].image}
-                        alt="이미지"
-                        style={{ width: "100%", height: "auto" }}
-                      />
+                      {quiz.image_list && quiz.image_list.length > 0 ? (
+                        <img
+                          src={quiz.image_list[0].image} // 템플릿 리터럴로 이미지 URL 처리
+                          alt="퀴즈 이미지"
+                          style={{ width: "90%", height: "auto" }}
+                          className="mx-auto d-block"
+                        />
+                      ) : (
+                        <p></p>
+                      )}
                     </Row>
 
-                    {/* 보기 섹션 */}
-                    <Row className="fw-bold h5">
-                      <ol>
-                        <li>ㄱ: {showAnswers ? quiz[0].answers[0] : " "}</li>{" "}
-                        <li>ㄴ: {showAnswers ? quiz[0].answers[1] : " "}</li>
-                        <li>ㄷ: {showAnswers ? quiz[0].answers[2] : " "}</li>
+                    <Row className="fw-bold h5 mb-1">
+                      <p className={`${quiz.image_list[0] ? "ms-5" : "ms-2"}`}>
+                        {quiz.content}
+                      </p>
+
+                      <ol className="p-3 ms-4 fw-normal">
+                        {quiz.answer.map((item, index) => (
+                          <li key={index}>
+                            <small>{showAnswers ? item.name : ""}</small>
+                          </li>
+                        ))}
                       </ol>
                     </Row>
 
-                    {/* 연습장 섹션 */}
-                    <Row className="mt-4 mb-2">
+                    <Row className="mb-2">
                       <InputGroup>
                         <InputGroup.Text>메모</InputGroup.Text>
-                        <Form.Control
-                          as="textarea"
-                          aria-label="With textarea"
-                        />
+                        <Form.Control as="textarea" aria-label="메모 영역" />
                       </InputGroup>
                     </Row>
 
-                    {/* 보기 버튼 섹션 */}
                     <Row className="text-center mt-3">
-                      {showAnswers ? (
-                        <Button variant="success" onClick={handleShowAnswers}>
-                         저장
-                        </Button>
-                      ) : (
-                        <Button variant="primary" onClick={handleShowAnswers}>
-                          정답 보기
-                        </Button>
-                      )}
+                      <Button
+                        variant={showAnswers ? "success" : "primary"}
+                        onClick={handleShowAnswers}
+                      >
+                        {showAnswers ? "저장" : "정답 보기"}
+                      </Button>
                     </Row>
                   </Row>
                 </Col>
 
                 {/* 다음 버튼 */}
-                <Col xs={1}>
+                <Col xs={1} onClick={handleNextPage}>
                   <Button className={styles.noBackground} variant="link">
                     <i
                       className={`${styles.arrow} ${styles.left}`}
