b30606416eefc25e1d56b2326a0329526e3e7e71
beak jong seoung <jjong015189@gmail.com>
2024-08-28 16:46:47 +0900

    restore 앱 생성 & 등록

--- gisa-backend/mysite/settings.py
+++ gisa-backend/mysite/settings.py
@@ -48,7 +48,7 @@ CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", default=[])
 
 third_apps = ["rest_framework", "corsheaders", "django_extensions"]
 
-local_apps = ["accounts", "quiz", "problem"]
+local_apps = ["accounts", "quiz", "problem", "restore"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
