906ac7d9ffd83b1c1cd7c9197ceba5723c56ca38
beak jong seoung <jjong015189@gmail.com>
2024-08-29 09:42:16 +0900

    restore 어드민 기본 페이지

--- gisa-backend/restore/admin.py
+++ gisa-backend/restore/admin.py
@@ -1,1 +1,22 @@
-# Register your models here.
+from django.contrib import admin
+from restore.models import Restore, RestoreAnswer, RestoreCategory, RestorePhoto
+
+
+@admin.register(RestoreCategory)
+class RestoreCategoeyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Restore)
+class RestoreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(RestoreAnswer)
+class RestoreAnswerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(RestorePhoto)
+class RestorePhotoAdmin(admin.ModelAdmin):
+    pass
