1c4ca95816d723a8b5ac68eb51eaf267686cb24e
beak jong seoung <jjong015189@gmail.com>
2024-09-02 17:41:36 +0900

    권한 설정

--- gisa-backend/core/permissions.py
+++ gisa-backend/core/permissions.py
@@ -1,40 +1,26 @@
+# 결제 기능 추가 시 주석 처리
+
 from accounts.models import Manager
-from core.models import MainCategory
-from payment.models import Subscription
-from rest_framework.exceptions import PermissionDenied
-from rest_framework.permissions import BasePermission
-
-
-def check_object_permissions(self, category_name):
-    obj = MainCategory.objects.get(name=category_name)
-
-    if not self.request.user.has_perm("view_maincategory", obj):
-        raise PermissionDenied("결제 페이지로 이동")
+from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
 class IsCategorySubscriber(BasePermission):
     def has_permission(self, request, view):
-        categoryName = request.query_params.get("categoryName")
-
-        try:
-            category = MainCategory.objects.get(name=categoryName)
-        except MainCategory.DoesNotExist:
-            return False
-
-        try:
-            Manager.objects.get(user=request.user, category=category)
+        if request.method in SAFE_METHODS:
             return True
-        except Manager.DoesNotExist:
-            try:
-                Subscription.objects.get(user=request.user, category=category, is_active=True)
 
-                # 초기에는 결제 기능 x
-                # 나중에 사용하려면 아래 코드를 활성화하세요.
-                # if subscription.expiry_date < timezone.now():
-                #     subscription.is_active = False
-                #     subscription.save()
-                #     return False
+        if Manager.objects.filter(user=request.user).exists():
+            return True
 
-                return True
-            except Subscription.DoesNotExist:
-                return False
+        return False
+
+        # 초기에는 결제 기능 x
+        # 나중에 사용하려면 아래 코드를 활성화하세요.
+        # try:
+        #     return Manager.objects.filter(user=request.user, category=category).exists()
+        # except Manager.DoesNotExist:
+        #     subscription = Subscription.objects.get(user=request.user, category=category, is_active=True)
+        #     if subscription.expiry_date < timezone.now():
+        #         subscription.is_active = False
+        #         subscription.save()
+        #         return False
