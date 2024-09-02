c18cea318d44dbc91464065989f3cef8a42024c6
beak jong seoung <jjong015189@gmail.com>
2024-09-02 17:42:10 +0900

    관리자 여부에 따라 리턴값 변경

--- gisa-frontend/src/components/wp/CategoryItem.js
+++ gisa-frontend/src/components/wp/CategoryItem.js
@@ -3,7 +3,7 @@ import { ListGroup, Button } from "react-bootstrap";
 
 const DONE_STYLE = { textDecoration: "line-through" };
 
-const CategoryItem = ({ category, onClick, isManager }) => (
+const CategoryItem = ({ index, category, onClick, isManager, deleteVersion, editVersion }) => (
   <ListGroup.Item className="d-flex justify-content-between align-items-start">
     <div
       style={{
@@ -14,36 +14,26 @@ const CategoryItem = ({ category, onClick, isManager }) => (
     >
       {category.version}
     </div>
-    
-    {/* 관리자일 경우 추가/수정/삭제 버튼 추가 */}
+
     {isManager && (
       <div>
         <Button
           variant="outline-secondary"
           size="sm"
           className="me-2"
-          onClick={(e) => {
-            e.stopPropagation(); // 부모 div의 onClick이 실행되지 않도록 함
-            console.log(`수정 클릭됨: ${category.version}`);
-            // 수정 버튼 클릭 시 로직 추가
-          }}
+          onClick={() => editVersion( index )}
         >
           수정
         </Button>
         <Button
           variant="outline-danger"
           size="sm"
-          onClick={(e) => {
-            e.stopPropagation(); // 부모 div의 onClick이 실행되지 않도록 함
-            console.log(`삭제 클릭됨: ${category.version}`);
-            // 삭제 버튼 클릭 시 로직 추가
-          }}
+          onClick={() => deleteVersion( index )}
         >
           삭제
         </Button>
       </div>
     )}
-
   </ListGroup.Item>
 );
 
