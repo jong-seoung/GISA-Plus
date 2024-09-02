7a1162e06341052c0a99b599961d6902c87ce6f3
beak jong seoung <jjong015189@gmail.com>
2024-09-02 11:15:36 +0900

    hotfix: 임시 권한 url 경로

--- gisa-backend/core/urls.py
+++ gisa-backend/core/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("category-list", MainCategoryListView.as_view(), name="자격증 목록"),
-    path("temporary-permission", temporary_permission.as_view(), name="권한 부여"),
+    path("temporary-permission", temporary_permission, name="권한 부여"),
 ]
