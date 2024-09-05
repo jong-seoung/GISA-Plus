7154b7f9328e32a6c384b4d3958eac0edc0f6761
beak jong seoung <jjong015189@gmail.com>
2024-09-05 15:07:11 +0900

    restore create, update, delete

--- gisa-backend/restore/views.py
+++ gisa-backend/restore/views.py
@@ -1,22 +1,40 @@
-from core.mixins import ActionBasedViewSetMixin
+from core.models import MainCategory
 from core.permissions import IsCategorySubscriber
-from rest_framework import viewsets
+from rest_framework.exceptions import ValidationError
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 from restore.models import Restore, RestoreCategory
-from restore.serializers import ManagerRestoreSerializer, RestoreCategoryListSerializer, RestoreListSerializer
+from restore.serializers import RestoreCategoryListSerializer, RestoreListSerializer
 
 
-class RestoreCategoryListView(ListAPIView):
+class RestoreCategoryListView(ModelViewSet):
     queryset = RestoreCategory.objects.all()
     serializer_class = RestoreCategoryListSerializer
     permission_classes = [IsAuthenticated, IsCategorySubscriber]
 
-    def get_queryset(self, *args, **kwargs):
+    def list(self, request, *args, **kwargs):
         category_name = self.request.query_params.get("categoryName", None)
-        self.queryset = self.queryset.filter(main_category__name=category_name)
-        return self.queryset
+        queryset = self.get_queryset()
+
+        if category_name:
+            queryset = queryset.filter(main_category__name=category_name)
+
+        self.queryset = queryset
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        category_name = request.data.get("categoryName")
+
+        if not category_name:
+            raise ValidationError("categoryName 쿼리 파라미터가 필요합니다.")
+
+        main_category = MainCategory.objects.get(name=category_name)
+
+        request.data["main_category"] = main_category.id
+
+        return super().create(request, *args, **kwargs)
 
 
 class RestoreView(ListAPIView):
@@ -33,14 +51,3 @@ class RestoreView(ListAPIView):
         serializer = self.serializer_class(self.queryset, many=True)
 
         return Response(serializer.data)
-
-
-# Manager
-class ManagerRestoreView(ActionBasedViewSetMixin, viewsets.ModelViewSet):
-    queryset = Restore.objects.all()
-    queryset_map = {
-        "list": ManagerRestoreSerializer.get_optimized_queryset(),
-        "partial_update": ManagerRestoreSerializer.get_optimized_queryset(),
-        "destroy": Restore.objects.all(),
-    }
-    serializer_class = ManagerRestoreSerializer
